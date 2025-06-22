d1e30863e60b521060a4ebdd38d183ad73c35b52
Joshuaburle <11491@holbertonstudents.com>
2025-06-22 12:45:28 +0200

    Write a function that adds two integers and returns the result.

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,12 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - entry point to test times_table
+ * main - test add function
  *
  * Return: Always 0
  */
 int main(void)
 {
-    times_table();
+    int sum;
+
+    sum = add(5, 3);
+    printf("5 + 3 = %d\n", sum);
+
+    sum = add(-4, 10);
+    printf("-4 + 10 = %d\n", sum);
+
     return (0);
 }
