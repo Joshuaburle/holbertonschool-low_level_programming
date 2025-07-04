da7e2a986495209801da19adb3cf324590489135
Joshuaburle <11491@holbertonstudents.com>
2025-07-04 13:09:02 +0200

    0. It ain't what they call you, it's what you answer to

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    if (argc > 0) {
-        printf("%s\n", argv[0]);
-    }
-    return 0;
+/**
+ * main - Prints the program name, including the path.
+ * @argc: Argument count.
+ * @argv: Argument vector (argv[0] is the program name).
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
 }
