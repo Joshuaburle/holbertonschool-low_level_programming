c1d550599dba0ff34b26b305ced9e8e25129462e
Joshuaburle <11491@holbertonstudents.com>
2025-07-04 08:23:56 +0200

    change again

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,11 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s[0] != '\0')
+	if (*s == '\0')
 	{
-		_putchar(s[0]);
-		_puts_recursion(s + 1);
+		putchar('\n');
+		return;
 	}
-	else
-		_putchar('\n');
+
+	putchar(*s);
+	_puts_recursion(s + 1);
 }
