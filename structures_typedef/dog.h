074bfdb61890d7747e537fedd0d63357caa67336
Joshuaburle <11491@holbertonstudents.com>
2025-07-11 13:04:54 +0200

    implement dog structure and associated functions for initialization and printing

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a new type for dog information
+ * @name: name
+ * @age: age
+ * @owner: oowner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif /* DOG_H */
