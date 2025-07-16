c31fb5e29a455ddca7068b77e3f0070ee972b9d1
Joshuaburle <11491@holbertonstudents.com>
2025-07-16 18:00:49 +0200

    style: standardize indentation and improve function documentation in function pointers

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,41 +1,63 @@
 #include "3-calc.h"
 
 /**
- * op_add - Returns the sum of a and b
+ * op_add - Returns the sum of two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of a + b.
  */
 int op_add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
 
 /**
- * op_sub - Returns the difference of a and b
+ * op_sub - Returns the difference of two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of a - b.
  */
 int op_sub(int a, int b)
 {
-    return (a - b);
+	return (a - b);
 }
 
 /**
- * op_mul - Returns the product of a and b
+ * op_mul - Returns the product of two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of a * b.
  */
 int op_mul(int a, int b)
 {
-    return (a * b);
+	return (a * b);
 }
 
 /**
- * op_div - Returns the result of the division of a by b
+ * op_div - Returns the result of dividing one integer by another.
+ * @a: The dividend.
+ * @b: The divisor.
+ *
+ * Return: The result of a / b.
+ *         If b is 0, the behavior is undefined (handled elsewhere).
  */
 int op_div(int a, int b)
 {
-    return (a / b);
+	return (a / b);
 }
 
 /**
- * op_mod - Returns the remainder of the division of a by b
+ * op_mod - Returns the remainder of the division of two integers.
+ * @a: The dividend.
+ * @b: The divisor.
+ *
+ * Return: The result of a % b.
+ *         If b is 0, the behavior is undefined (handled elsewhere).
  */
 int op_mod(int a, int b)
 {
-    return (a % b);
+	return (a % b);
 }
