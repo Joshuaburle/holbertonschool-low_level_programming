3cb7112cf11596ea7f591a32f958a2061732c1ea
Joshuaburle <11491@holbertonstudents.com>
2025-07-30 11:37:45 +0200

    style: format code for consistency and readability across linked list functions

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,24 +11,24 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    char *dup_str;
+	list_t *new_node;
+	char *dup_str;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    dup_str = strdup(str);
-    if (dup_str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->str = dup_str;
-    new_node->len = strlen(dup_str);
-    new_node->next = *head;
-    *head = new_node;
+	new_node->str = dup_str;
+	new_node->len = strlen(dup_str);
+	new_node->next = *head;
+	*head = new_node;
 
-    return (new_node);
+	return (new_node);
 }
--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,34 +11,34 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *temp;
-    char *dup_str;
+	list_t *new_node, *temp;
+	char *dup_str;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    dup_str = strdup(str);
-    if (dup_str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->str = dup_str;
-    new_node->len = strlen(dup_str);
-    new_node->next = NULL;
+	new_node->str = dup_str;
+	new_node->len = strlen(dup_str);
+	new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
-    temp = *head;
-    while (temp->next != NULL)
-        temp = temp->next;
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
 
-    temp->next = new_node;
-    return (new_node);
+	temp->next = new_node;
+	return (new_node);
 }
