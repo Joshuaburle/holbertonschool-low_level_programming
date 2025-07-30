32eef9be082cfd6bbd37706f0de3bf98b5b53b7d
Joshuaburle <11491@holbertonstudents.com>
2025-07-30 12:03:50 +0200

    refactor: replace strdup with custom string duplication in add_node_end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,32 +1,70 @@
 #include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+
+/**
+ * _strlen - calculates the length of a string
+ * @s: string to measure
+ *
+ * Return: length of the string
+ */
+static unsigned int _strlen(const char *s)
+{
+	unsigned int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * _strdup - duplicates a string
+ * @str: string to duplicate
+ *
+ * Return: pointer to the new string, or NULL if fails
+ */
+static char *_strdup(const char *str)
+{
+	unsigned int len = _strlen(str);
+	char *dup = malloc(len + 1);
+	unsigned int i;
+
+	if (!dup)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+	dup[len] = '\0';
+
+	return (dup);
+}
 
 /**
  * add_node_end - adds a new node at the end of a list_t list
  * @head: double pointer to the head of the list
- * @str: string to put in the new node
+ * @str: string to store in the new node
  *
  * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *temp;
-	char *dup_str;
+	unsigned int len;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	if (!str)
 		return (NULL);
 
-	dup_str = strdup(str);
-	if (dup_str == NULL)
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+
+	len = _strlen(str);
+	new_node->str = _strdup(str);
+	if (!new_node->str)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
-	new_node->str = dup_str;
-	new_node->len = strlen(dup_str);
+	new_node->len = len;
 	new_node->next = NULL;
 
 	if (*head == NULL)
@@ -40,5 +78,6 @@ list_t *add_node_end(list_t **head, const char *str)
 		temp = temp->next;
 
 	temp->next = new_node;
+
 	return (new_node);
 }
