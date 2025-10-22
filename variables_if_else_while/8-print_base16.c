35a6980696068f9bb2670b03e4ce838fe293eeaa
Joshuaburle <11491@holbertonschool.com>
2025-10-22 10:19:12 +0200

    Print base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+{
+		putchar(num);
+}
+{
+	char let;
+
+	for (let = 'a'; let <= 'f'; let++)
+
+		putchar(let);
+}
+	putchar ('\n');
+	return (0);
+}
