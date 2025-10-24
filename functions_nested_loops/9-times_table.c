986b49fd88cb7d65fe1c86ac384f52213a0b5036
Joshuaburle <11491@holbertonschool.com>
2025-10-24 17:00:02 +0200

    Print all table since 0 to 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *times_table - print every * table from 0 to 9
+ *
+ *
+ * return: always 0 on sucess
+ */
+
+void times_table(void)
+{
+	int m;
+	int p;
+
+	for (m = 0; m <= 9; m++)
+	{
+		for (p = 0; p <= 9; p++)
+		{
+			if (m * p > 9)
+			{
+				_putchar(m * p / 10 + '0');
+				_putchar(m * p % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(m * p + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+	_putchar('\n');
+	}
+}
