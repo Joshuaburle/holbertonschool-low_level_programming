a2bf515879d705236bca361c6c12936e5bcffce3
Joshuaburle <11491@holbertonschool.com>
2025-10-24 09:21:56 +0200

    Write a function to return an absolut number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_sign - make a function to know the sign
+ *@n: check what return 0
+ *
+ * Return: 1 positive 0 equal -1 negative
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+	}
+	return(n);
+}
+
