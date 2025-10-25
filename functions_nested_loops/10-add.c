85dcbf1daa597f2a8dcac89ffc8230b00937cac7
Joshuaburle <11491@holbertonschool.com>
2025-10-25 13:13:18 +0200

    Create a function that adds 2 int and returns the results

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *@a: int a
+ *@b: int b
+ *
+ *Return: return the result
+ */
+
+int add(int a, int b)
+{
+	int n = a + b;
+
+	return (n);
+}
+
+
--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -22,6 +22,7 @@ void times_table(void)
 				_putchar(m * p % 10 + '0');
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
 			}
 			else
 			{
