946f736b638e9d779250ef09f77b26cd2c3f8a1b
Joshuaburle <11491@holbertonschool.com>
2025-10-27 12:25:28 +0100

    unction that prints the numbers, from 0 to 9, followed by a new line

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - function that prints the numbers, from 0 to 9 pass 2 & 4
+ *
+ *Description: function that prints the numbers from 0 to 9 pass 2 & 4 new line
+ *section header:
+ *Return: always 0 on sucess
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c != '2' && c != '4')
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
