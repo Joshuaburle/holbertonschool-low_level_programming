182b1fc9608f3259c694e1173411a37a5d1d97ac
Joshuaburle <11491@holbertonschool.com>
2025-10-28 07:29:42 +0100

    print a square

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *print_line - function that draws a straight line in the terminal
+ *
+ *Description: function that draws a straight line in the terminal
+ *@n: int n
+ *section header:
+ *Return: always 0 on sucess
+ */
+
+void print_diagonal(int n)
+{
+    int i;
+    int j;
+
+    if (n <= 0)
+    {
+		_putchar('\n');
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+        {
+            _putchar(' ');
+        }
+		_putchar('\\');
+        _putchar('\n');
+    }
+}
