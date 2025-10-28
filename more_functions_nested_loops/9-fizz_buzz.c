699410a9e55d60ac7fc72fee7a4524da204c8e68
Joshuaburle <11491@holbertonschool.com>
2025-10-28 14:21:24 +0100

    Update

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -18,12 +18,16 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
+
 		else if (i % 3 == 0)
 			printf("Fizz ");
+
 		else if (i == 100)
 			printf("Buzz\n");
+
 		else if (i % 5 == 0)
 			printf("Buzz ");
+		
 		else
 			printf("%d ", i);
 	}
