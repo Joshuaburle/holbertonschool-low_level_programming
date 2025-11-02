14077f4fefd26e562df33b0cd737b1c9d60cada6
Joshuaburle <11491@holbertonschool.com>
2025-11-02 12:40:54 +0100

    Add 2-strncpy.c file

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+/**
+ * _strncpy - copy string
+ * @dest: destination buffer
+ * @src: source string
+ * @n: maximum bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
