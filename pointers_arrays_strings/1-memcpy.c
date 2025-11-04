578e192059dcf8dd61da3a76913b13f04765ee56
Joshuaburle <11491@holbertonschool.com>
2025-11-04 10:47:33 +0100

    Update the loop of 1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -18,7 +18,5 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for (; i < n; i++)
-		dest[i] = '\0';
-
-	return (src);
+	return (dest);
 }
