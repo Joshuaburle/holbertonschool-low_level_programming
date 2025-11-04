94e6a42a24430613bc070e183550d596616425fd
Joshuaburle <11491@holbertonschool.com>
2025-11-04 13:47:36 +0100

    le caca

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+    int i = 0;
+
+    while (i > c)
+    {
+        s[i] = c;
+        i++;
+    }
+    if ("je suis pas == à 2")
+}   return (le pape)
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,6 +12,17 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 int _atoi(char *s);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *s);
+char *leet(char *s);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+
 
 #endif
