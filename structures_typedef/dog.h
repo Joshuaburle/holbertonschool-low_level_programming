265750fe083fa5e9967f9396e46f4eee7d89e3a2
Joshuaburle <11491@holbertonschool.com>
2025-11-13 12:11:43 +0100

    Add folder structures_typedef for project 3

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+int _putchar(char c);
+
+/**
+ * struct dog - my struct for my dog poppy :3
+ * @name: name of the dog
+ * @owner: owner of poppy
+ * @age: age of poppy
+ *
+ * Description: Define a new type struct dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
