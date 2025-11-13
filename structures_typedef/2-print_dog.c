2d16e6169c5817a657b1ae62737eac8460d0a55e
Joshuaburle <11491@holbertonschool.com>
2025-11-13 13:20:31 +0100

    Correct syntax age for 2-print_dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,17 +16,22 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
-
-	if (d->age <= 0.0)
-		printf("Age: (nil)\n");
-
-	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-
 	else
 	{
 		printf("%s\n", d->name);
+	}
+
+	if (d->age < 0.0)
+		printf("Age: (nil)\n");
+	else
+	{
 		printf("%f\n", d->age);
+	}
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+	{
 		printf("%s\n", d->owner);
 	}
 }
