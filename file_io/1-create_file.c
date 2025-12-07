99033f00f14dd42ae677878716d298bba19f97b8
Joshuaburle <11491@holbertonschool.com>
2025-12-07 11:11:11 +0100

    Rework all code of file 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 /**
- * create_file - creates a file
- * @filename: name of the file to create
- * @text_content: NULL terminated string to write to the file
+ * create_file - Creates a file with rw------- permissions
+ * @filename: Name of the file to create
+ * @text_content: NULL-terminated string to write into the file
  *
  * Return: 1 on success, -1 on failure
  */
@@ -16,24 +17,21 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len]; len++)
-			;
-	}
-
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		w = write(fd, text_content, len);
-		if (w == -1 || w != len)
-		{
-			close(fd);
-			return (-1);
-		}
+		while (text_content[len])
+			len++;
+	}
+
+	w = write(fd, text_content, len);
+	if (w == -1)
+	{
+		close(fd);
+		return (-1);
 	}
 
 	close(fd);
